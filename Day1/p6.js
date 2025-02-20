d5a858265272b04aed470bd8fbb28fd2a5257768
23ayushigupta <ayushiguptasln@gmail.com>
2025-02-20 14:53:05 +0530

    Day1 fs promises

--- Day1/p6.js
+++ Day1/p6.js
@@ -1,11 +1,12 @@
-const fs=reqire('fs');
+const fs = require('fs');
 
-const write=()=>{
-    fs.writeFile=("./data.txt",data,(err)=>{
+const write=(data)=>{
+    fs.writeFile("./mydir/data.txt",data,(err)=>{
         if(err)
             console.log("Error Writing file",err);
-        elseconsole.log("file written successfully");
+        else
+        console.log("file written successfully");
 
     });
 }
-write("T am new data")
+write("Ayushi")
